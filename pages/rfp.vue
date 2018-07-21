7c6fce37f50486dc8e05925c217e7934f77f099c
Peter Menocal <petermenocal@pm.CoOperate>
2018-07-21 09:39:46 -0700

    update

--- pages/rfp.vue
+++ pages/rfp.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app" class="container" style="font-family: arial !important">
     <the-navigation-bar></the-navigation-bar>
-    <div class="fl w-75 tc" style="min-height: 1000px">
+
+    <div class="fl w-100 tc" style="min-height: 1000px">
       <v-stepper v-model="e6" vertical non-linear>
         <v-stepper-step editable :complete="e6> 1" step="1"> Your information
         </v-stepper-step>
@@ -38,112 +39,70 @@
             </v-flex>
           </v-layout>
           <v-card flat color="grey lighten-1" class="mb-5" height="420px">
-
-            <v-layout align-start justify-start column>
-              <GmapMap :center="{lat:36.181271, lng:-115.134132} " :zoom=11 map-type-id="terrain" style="width: 100%; height:100%; ">
-                <GmapMarker :key="index " v-for="(m, index) in markers " :position="m.position " :clickable="true " :draggable="true " @click="center=m.position" />
-              </GmapMap>
-            </v-layout>
+            <GmapMap :center="{lat:36.181271, lng:-115.134132}" :zoom=11 map-type-id="terrain" style="width: 100%; height:100%; ">
+              <GmapMarker :key="index " v-for="(m, index) in markers " :position="m.position " :clickable="true " :draggable="true " @click="center=m.position" />
+            </GmapMap>
           </v-card>
           <v-btn color="primary " @click="e6=3 ">Continue</v-btn>
           <v-btn flat>Cancel</v-btn>
-
         </v-stepper-content>
-
         <v-stepper-step editable :complete="e6> 3" step="3">Date Requirements</v-stepper-step>
-
         <v-stepper-content step="3">
           <v-card flat color="grey lighten-1" class="mb-5 flex flex-column items-start justify-start" height="100%">
             <h1>When is your event?</h1>
-            <v-btn class="success" @click="datePickers++">Add another date</v-btn>
-            <span class="flex flex-row flex-wrap space-between">
-              <span v-for="i in datePickers" class="w-third mb3">
-                <v-date-picker v-model="datePicker" :landscape="landscape" :reactive="reactive" :min="min"></v-date-picker>
+            <div class="text-xs-center">
+              <v-bottom-sheet v-model="sheet">
+                <v-btn slot="activator" color="purple" dark>
+                  Add another date
+                </v-btn>
+                <v-list>
+                  <v-subheader>Which date style?</v-subheader>
+                  <v-list-tile v-for="tile in tiles" :key="tile.title" @click="selectDateStyle(tile)">
+                    <span class="w-10 b ttu">{{ tile.title }}</span>
+                    <v-list-tile-title>
+                      ({{ tile.description }})</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-bottom-sheet>
+            </div>
+            <span class="flex flex-row flex-wrap justify-between ">
+              <span v-for="i in datePickers " class="w-50 mb3 ">
+                <v-date-picker v-model="datePicker " :landscape="true " :reactive="reactive " :min="min "></v-date-picker>
               </span>
             </span>
           </v-card>
-          <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+          <v-btn color="primary " @click="e6=4 ">Continue</v-btn>
           <v-btn flat>Cancel</v-btn>
         </v-stepper-content>
 
-        <v-stepper-step editable step="4">Space requirements</v-stepper-step>
-        <v-stepper-content step="4">
-          <v-card flat color="grey lighten-1" class="mb-5" height="420px">
+        <v-stepper-step editable step="4 ">Space requirements</v-stepper-step>
+        <v-stepper-content step="4 ">
+          <v-card flat color="grey lighten-1 " class="mb-5 " height="420px ">
+            <v-card-title primary-title class="tl ">
+              <h3 class="headline mb-0 fl w-100 ">Attendance requirements</h3>
+              Adjust the sliders below to denote your projected attendees.
+            </v-card-title>
             <v-container fluid grid-list-lg>
-              <v-layout row wrap>
-                <v-flex>
-                  <v-range-slider v-model="price" :max="600" :min="20" :step="10"></v-range-slider>
+              <v-layout row wrap v-for="i in datePickers ">
+                <div class="tc flex flex-row items-center justify-center w-100 ">
+                  <v-icon class="mr2 ">calendar_today</v-icon>
+                  <h3 class="fw4 ">{{datePicker}}</h3>
+                </div>
+                <v-flex shrink style="width: 60px ">
+                  <v-text-field v-model="price[0] " class="mt-0 " hide-details single-line type="number "></v-text-field>
                 </v-flex>
-                <v-flex shrink style="width: 60px">
-                  <v-text-field v-model="price[1]" class="mt-0" hide-details single-line type="number"></v-text-field>
+                <v-flex>
+                  <v-range-slider v-model="price " :max="600 " :min="20 " :step="10 "></v-range-slider>
+                </v-flex>
+                <v-flex shrink style="width: 60px ">
+                  <v-text-field v-model="price[1] " class="mt-0 " hide-details single-line type="number "></v-text-field>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card>
-          <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
-          <v-btn flat>Cancel</v-btn>
+          <nuxt-link to="/rfp2">Complete your RFP</nuxt-link>
         </v-stepper-content>
       </v-stepper>
-    </div>
-    <div class="fl w-25 ph3">
-      <h1>Preview</h1>
-      <v-expansion-panel>
-        <v-expansion-panel-content>
-          <div slot="header">Contact Information</div>
-          <v-card>
-            <v-card-text>
-              <strong>Name: </strong> {{ name }} <br>
-              <strong>Email: </strong> {{ email }} <br>
-              <strong>Organization: </strong> {{ organization }} <br>
-              <strong>Phone: </strong> {{ phone }} <br>
-            </v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-content>
-          <div slot="header">Location</div>
-          <v-card>
-            <v-card-text>
-              <v-list two-line>
-                <template v-for="(item, index) in places">
-                  <v-list-tile>
-                    <v-list-tile-avatar>
-                      <img src="//placehold.it/500x500">
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                      <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                      <v-list-tile-sub-title v-html="item.formatted_address"></v-list-tile-sub-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </template>
-              </v-list>
-            </v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-content>
-          <div slot="header">Date requirements</div>
-          <v-card>
-            <v-card-text>
-              <v-list two-line>
-                <template v-for="(item, index) in places">
-                  <v-list-tile>
-                    <v-list-tile-avatar>
-                      <img src="//placehold.it/500x500">
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                      <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                      <v-list-tile-sub-title v-html="item.formatted_address"></v-list-tile-sub-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </template>
-              </v-list>
-            </v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
     </div>
   </div>
 </template>
@@ -158,15 +117,16 @@
   data () {
       return {
         price: [110, 440],
-        e6: 4,
+        e6: 1,
+        sheet: false,
         datePickers: 1,
         picker: '',
         address: '',
         markers: [],
-        email: '',
-        name: "",
-        organization: "",
-        phone: "",
+        email: 'e.stone@rfptester.com',
+        name: "Emma Stone",
+        organization: "RFP tester",
+        phone: "702-555-5555",
         emailRules: [],
         nameRules: [],
         place: null,
@@ -175,10 +135,22 @@
         reactive: true,
         datePicker: "2018-07-01",
         places: [],
-        min: "2018-07-01"
+        min: "2018-07-01",
+        dateStyle:  { title: 'or', description: 'My event can land on either of the dates I select.'},
+        tiles: [
+          { title: 'or', description: 'My event can land on either of the dates I select.'},
+          { title: 'and', description: 'My event must land on both of the dates I select.'},
+          { title: 'through', description: 'My event takes place through the selected dates.'},
+          { title: 'cherry pick', description: 'My event has a special pattern. All dates must be available individually.'},
+        ]
       }
     },
       methods: {
+        selectDateStyle(tile) {
+          this.dateStyle = tile
+          this.datePickers++
+          this.sheet = false;
+        },
       setDescription(description) {
         this.description = description;
       },
---
+++ pages/rfp2.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="fl w-100">
+    <h1>rfp wizard</h1>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click.native="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Reason for bidding</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click.native="dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list three-line subheader>
+          <v-subheader>James Bond posted:</v-subheader>
+          <v-list-tile avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Booked solid through december</v-list-tile-title>
+              <v-list-tile-sub-title>Sorry! We have no availability for the dates you mentioned.</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list three-line subheader>
+          <v-subheader>General</v-subheader>
+          <v-list-tile avatar>
+            <v-list-tile-action>
+              <v-checkbox v-model="notifications"></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Option</v-list-tile-title>
+              <v-list-tile-sub-title>Lorem ipsum dolor sit amet consectetur adipisicing elit.</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-action>
+              <v-checkbox v-model="sound"></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Option</v-list-tile-title>
+              <v-list-tile-sub-title>Lorem ipsum dolor sit amet consectetur adipisicing elit.</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-action>
+              <v-checkbox v-model="widgets"></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Option</v-list-tile-title>
+              <v-list-tile-sub-title>Lorem ipsum dolor sit amet consectetur adipisicing elit.</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <div class="w-50 center">
+          <v-textarea name="input-7-1" label="Send a reply to James Bond" value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through." hint="Reps will receivve this comment in their profile inbox."></v-textarea>
+          <v-btn @click="dialog = false">Send reply</v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
+    <v-data-table :headers="headers" :items="desserts" class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.value }}</td>
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.address }}</td>
+        <td class="text-xs-right">{{ props.item.sales_contact }}</td>
+        <td class="text-xs-right">{{ props.item.sales_contact_email }}</td>
+        <td class="text-xs-right">{{ props.item.sales_contact_phone }}</td>
+        <td class="text-xs-right">{{ props.item.sales_contact_profile }}</td>
+        <td class="text-xs-right">{{ props.item.bidding }}</td>
+        <td class="text-xs-right">
+          <v-btn @click="dialog = true">view</v-btn>
+        </td>
+        <td class="text-xs-right">{{ props.item.total_rooms }}</td>
+        <td class="text-xs-right">{{ props.item.aaa_rating }}</td>
+        <td class="text-xs-right">{{ props.item.forbes_rating }}</td>
+        <td class="text-xs-right">{{ props.item.available_rates }}</td>
+        <td class="text-xs-right">{{ props.item.preferred_dates }}</td>
+        <td class="text-xs-right">{{ props.item.alternative_dates }}</td>
+        <td class="text-xs-right">{{ props.item.comissionable }}</td>
+        <td class="text-xs-right">{{ props.item.meeting_rooms_no }}</td>
+        <td class="text-xs-right">{{ props.item.avg_breakfast }}</td>
+        <td class="text-xs-right">{{ props.item.avg_lunch }}</td>
+        <td class="text-xs-right">{{ props.item.avg_dinner }}</td>
+        <td class="text-xs-right">{{ props.item.avg_buffet }}</td>
+        <td class="text-xs-right">{{ props.item.avg_reception }}</td>
+        <td class="text-xs-right">{{ props.item.avg_break }}</td>
+        <td class="text-xs-right">{{ props.item.fb_min }}</td>
+        <td class="text-xs-right">{{ props.item.meeting_room_fee }}</td>
+        <td class="text-xs-right">{{ props.item.closest_airport_distance }}</td>
+        <td class="text-xs-right">{{ props.item.airport_shuttle }}</td>
+        <td class="text-xs-right">{{ props.item.airport_shuttle_cost }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Response',
+            align: 'left',
+            sortable: false,
+            value: 'Response'
+          },
+          { text: 'Venue name', value: 'venue' },
+          { text: 'Venue address', value: 'address' },
+          { text: 'Sales contact', value: 'sales_contact' },
+          { text: 'Contact email', value: 'sales_contact_email' },
+          { text: 'Contact phone', value: 'sales_contact_phone' },
+          { text: 'Contact profile', value: 'sales_contact_profile' },
+          { text: 'Are you bidding?', value: 'bidding' },
+          { text: 'Reason for not bidding', value: 'bidding_reason' },
+          { text: 'Total rooms', value: 'total_rooms' },
+          { text: 'AAA Rating', value: 'aaa_rating' },
+          { text: 'Forbes Rating', value: 'forbes_rating' },
+          { text: 'Preferred Dates', value: 'forbes_rating' },
+          { text: 'Available Rates', value: 'forbes_rating' },
+          { text: 'Alternatie Dates', value: 'forbes_rating' },
+          { text: 'Commissionable to client', value: 'forbes_rating' },
+          { text: 'Number of meeting rooms', value: 'forbes_rating' },
+          { text: 'Avg. Breakfast', value: 'forbes_rating' },
+          { text: 'Avg. Lunch', value: 'forbes_rating' },
+          { text: 'Avg. Buffet', value: 'forbes_rating' },
+          { text: 'Avg. Dinner', value: 'forbes_rating' },
+          { text: 'Avg. Reception', value: 'forbes_rating' },
+          { text: 'Avg. Break', value: 'forbes_rating' },
+          { text: 'F&B Min', value: 'forbes_rating' },
+          { text: 'Meeting Room Rental Fee', value: 'forbes_rating' },
+          { text: 'Closest Airport Distance', value: 'forbes_rating' },
+          { text: 'Airport shuttle', value: 'forbes_rating' },
+          { text: 'Airport shuttle cost', value: 'forbes_rating' },
+        ],
+        dialog: false,
+        desserts: [
+          {
+            value: false,
+            name: 'Hotel Name',
+            address: '123 Address',
+            sales_contact: "James Bond",
+            sales_contact_email: "j@j.com",
+            sales_contact_phone: "702-555-5555",
+            sales_contact_profile: "link",
+            bidding: false,
+            bidding_reason: "Reason",
+            total_rooms: 10005,
+            aaa_rating: 5,
+            forbes_rating: 5,
+            available_rates: "$30/night",
+            preferred_dates: "link",
+            alternative_dates: "link",
+            comissionable: true,
+            meeting_rooms_no: 5555,
+            avg_breakfast: "$9",
+            avg_lunch: "$20",
+            avg_dinner: "$30",
+            avg_buffet: "$20",
+            avg_reception: "$50",
+            avg_break: "$100",
+            fb_min: "$1001",
+            meeting_room_fee: "$192",
+            closest_airport_distance: "4km",
+            airport_shuttle: false,
+            airport_shuttle_cost: "50"
+          }
+        ]
+      }
+    },
+     created: function () {
+      var self = this;
+      var hotels = ['Hilton', 'Hard Rock', 'Luxor', 'MGM', 'Four Seasons']
+      var names = ['Jim Bob', 'Jane Doe', 'Sarah Barbara', 'Nick Cole', 'Sylvia Joshlyn']
+      setInterval(function () {
+         self.$data.desserts.push({
+            value: true,
+            name: hotels[Math.floor(Math.random() * 4)],
+            address: '123 Address',
+            sales_contact: names[Math.floor(Math.random() * 4)],
+            sales_contact_email: "j@j.com",
+            sales_contact_phone: "702-555-5555",
+            sales_contact_profile: "link",
+            bidding: false,
+            bidding_reason: "Reason",
+            total_rooms: Math.floor(Math.random() * 101),
+            aaa_rating: 5,
+            forbes_rating: 5,
+            available_rates: "$30/night",
+            preferred_dates: "link",
+            alternative_dates: "link",
+            comissionable: true,
+            meeting_rooms_no: 5555,
+            avg_breakfast: "$9",
+            avg_lunch: "$20",
+            avg_dinner: "$30",
+            avg_buffet: "$20",
+            avg_reception: "$50",
+            avg_break: "$100",
+            fb_min: "$1001",
+            meeting_room_fee: "$192",
+            closest_airport_distance: "4km",
+            airport_shuttle: false,
+            airport_shuttle_cost: "50"
+          })
+      }, 7000)
+    }, 
+    
+  }
+</script>
